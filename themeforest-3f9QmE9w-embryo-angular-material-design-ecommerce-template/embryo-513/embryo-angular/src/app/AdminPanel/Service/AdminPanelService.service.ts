9cf023d685986fd69cb204c26f0931ddf3ca322b
estherw <wwr1109@gmail.com>
2020-08-30 19:23:58 +0300

    Eti Perelman: DB & collabrationComponent

--- themeforest-3f9QmE9w-embryo-angular-material-design-ecommerce-template/embryo-513/embryo-angular/src/app/AdminPanel/Service/AdminPanelService.service.ts
+++ themeforest-3f9QmE9w-embryo-angular-material-design-ecommerce-template/embryo-513/embryo-angular/src/app/AdminPanel/Service/AdminPanelService.service.ts
@@ -1,88 +1,95 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs/Rx';
-import 'rxjs/Rx';
-import * as screenfull from 'screenfull';
-import { MatDialogRef, MatDialog } from '@angular/material';
-import { DeleteListDialogComponent} from '../Widget/PopUp/DeleteListDialog/DeleteListDialog.component';
-import { SeeListDialogComponent } from '../Widget/PopUp/SeeListDialog/SeeListDialog.component';
-import { AddNewUserComponent } from '../Widget/PopUp/AddNewUser/AddNewUser.component';
-import { AngularFireDatabase, AngularFireList, AngularFireObject } from "@angular/fire/database";
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs/Rx";
+import "rxjs/Rx";
+import * as screenfull from "screenfull";
+import { MatDialogRef, MatDialog } from "@angular/material";
+import { DeleteListDialogComponent } from "../Widget/PopUp/DeleteListDialog/DeleteListDialog.component";
+import { SeeListDialogComponent } from "../Widget/PopUp/SeeListDialog/SeeListDialog.component";
+import { AddNewUserComponent } from "../Widget/PopUp/AddNewUser/AddNewUser.component";
+import {
+  AngularFireDatabase,
+  AngularFireList,
+  AngularFireObject,
+} from "@angular/fire/database";
+import { environment } from "src/environments/environment";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
-
 export class AdminPanelServiceService {
+  sidenavOpen: boolean = true;
+  sidenavMode: string = "side";
+  chatSideBarOpen: boolean = true;
+  editProductData: any;
+  products: AngularFireObject<any>;
 
-	sidenavOpen 	 : boolean = true;
-	sidenavMode 	 : string = "side";
-	chatSideBarOpen : boolean = true;
-	editProductData : any;
-	products  : AngularFireObject<any>;
+  constructor(
+    private http: HttpClient,
+    private dialog: MatDialog,
+    private db: AngularFireDatabase
+  ) {}
 
-	constructor(private http:HttpClient,
-					private dialog: MatDialog,
-					private db: AngularFireDatabase) { }
-
-	/*
+  /*
 		---------- Pop Up Function ----------
 	*/
 
-	//deleteDiaglog function is used to open the Delete Dialog Component. 
-	deleteDialog(data:string){
-		let dialogRef : MatDialogRef<DeleteListDialogComponent>;
-		dialogRef = this.dialog.open(DeleteListDialogComponent);
-		dialogRef.componentInstance.data = data;
-		
-		return dialogRef.afterClosed();
-	}
+  //deleteDiaglog function is used to open the Delete Dialog Component.
+  deleteDialog(data: string) {
+    let dialogRef: MatDialogRef<DeleteListDialogComponent>;
+    dialogRef = this.dialog.open(DeleteListDialogComponent);
+    dialogRef.componentInstance.data = data;
 
-	//getProducts method is used to get the products.
-   public getProducts() {
-      this.products = this.db.object("products");
-      return this.products;
-   }
+    return dialogRef.afterClosed();
+  }
 
-	//getTableTabContent method is used to get the transcation table data.
-	getTableTabContent() {
-		let tableContent : any;
-		tableContent = this.db.object("transcationTable");
-		return tableContent;
-	}
+  //getProducts method is used to get the products.
+  public getProducts() {
+    this.products = this.db.object("products");
+    return this.products;
+  }
 
-	//getBuySellChartData method is used to get the buy and sell chart data.
-	getBuySellChartContent() {
-		let buySellChart : any;
-		buySellChart = this.db.list("buySellChartData");
-		return buySellChart;
-	}
+  //getTableTabContent method is used to get the transcation table data.
+  getTableTabContent() {
+    let tableContent: any;
+    tableContent = this.db.object("transcationTable");
+    return tableContent;
+  }
 
-	//getInvoiceContent method is used to get the Invoice table data.
-	getInvoiceContent() {
-		let invoiceList : any;
-		invoiceList = this.db.list("invoiceData");
-		return invoiceList;
-	}
+  //getBuySellChartData method is used to get the buy and sell chart data.
+  getBuySellChartContent() {
+    let buySellChart: any;
+    buySellChart = this.db.list("buySellChartData");
+    return buySellChart;
+  }
 
-	//getCollaborationContent method is used to get the Collaboration table data.
-	getCollaborationContent () {
-		let collaboration : any;
-		collaboration = this.db.list("collaborationData");
-		return collaboration;
-	}
+  //getInvoiceContent method is used to get the Invoice table data.
+  getInvoiceContent() {
+    let invoiceList: any;
+    invoiceList = this.db.list("invoiceData");
+    return invoiceList;
+  }
 
-	//seeList function is used to open the see Dialog Component.
-	seeList(){
-		let dialogRef : MatDialogRef<SeeListDialogComponent>;
-		dialogRef = this.dialog.open(SeeListDialogComponent);
-	}	
+  //getCollaborationContent method is used to get the Collaboration table data.
+  getCollaborationContent() {
+    return this.http.get(`${environment.baseUrl}collaborationDatas`);
+  }
 
-	//addNewUserDialog function is used to open Add new user Dialog Component. 
-	addNewUserDialog(){
-		let dialogRef : MatDialogRef<AddNewUserComponent>;
-		dialogRef = this.dialog.open(AddNewUserComponent);
-		
-		return dialogRef.afterClosed();
-	}
+  addNewCollabration(addUser) {
+    return this.http.post(`${environment.baseUrl}collaborationDatas`, addUser);
+  }
+
+  //seeList function is used to open the see Dialog Component.
+  seeList() {
+    let dialogRef: MatDialogRef<SeeListDialogComponent>;
+    dialogRef = this.dialog.open(SeeListDialogComponent);
+  }
+
+  //addNewUserDialog function is used to open Add new user Dialog Component.
+  addNewUserDialog() {
+    let dialogRef: MatDialogRef<AddNewUserComponent>;
+    dialogRef = this.dialog.open(AddNewUserComponent);
+
+    return dialogRef.afterClosed();
+  }
 }
